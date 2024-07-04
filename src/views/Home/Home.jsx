7600303a0ea7f0c0da783129725365c6dd04bddc
Gabriel <gabrielmaranhao.cpm@gmail.com>
2024-07-04 15:55:12 -0300

    initial commit

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import './index.scss'
+
+const Home = () => {
+  return (
+
+    <div className="home">
+        <h1>Home</h1>
+        <button>
+            <Link to={'/products/1'}>Products</Link>
+        </button>
+    </div>
+  
+  )
+}
+
+export default Home
